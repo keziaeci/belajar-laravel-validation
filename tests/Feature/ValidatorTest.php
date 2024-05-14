2340bec7d7332152521e955073de9b827533abe1
keziaeci <keziaeci2806@gmail.com>
2024-05-14 11:45:02 +0700

    Validation Exception

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -16,7 +17,7 @@ class ValidatorTest extends TestCase
     function testValidator() {
         $data = [
             'username' => 'admin',
-            'password' => ''
+            'password' => '123'
         ];
         
         $rules = [
@@ -48,5 +49,40 @@ class ValidatorTest extends TestCase
 
         assertFalse($validator->passes());
         assertTrue($validator->fails());
-    }   
+
+        $msg = $validator->getMessageBag();
+        $keys = $msg->keys();
+        $msg->get('username');
+        
+        Log::info($msg->toJson(JSON_PRETTY_PRINT));
+        // dd($keys);
+    }
+    
+    function testValidationException() {
+        $data = [
+            'username' => '',
+            'password' => ''
+        ];
+        
+        $rules = [
+            'username' => 'required',
+            'password' => 'required'
+        ];
+
+        
+        $validator = Validator::make($data,$rules);
+        assertNotNull($validator);
+
+        try {
+            $validator->validate();
+            self::fail('ValidationException not thrown');
+            // dd($validator->validate());
+        } catch (\Illuminate\Validation\ValidationException $exception) {
+            // dd($exception->validator);
+            assertNotNull($exception->validator);
+            // dd($exception->validator->errors());
+            Log::error($exception->validator->errors()->toJson(JSON_PRETTY_PRINT));
+        }
+    }
+
 }
